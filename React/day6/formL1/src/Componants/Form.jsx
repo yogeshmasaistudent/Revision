7d0c1edf40a245bf01a1911cc796f5ff706de1f3
Yogesh <kushawahyogesh93@gmail.com>
2024-04-15 10:55:27 +0530

    done

--- React/day6/formL1/src/Componants/Form.jsx
+++ React/day6/formL1/src/Componants/Form.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Form.module.css";
+import Table from "./Table";
 
 function Form() {
   const [showPassword, setShowPassword] = useState(false);
@@ -8,45 +9,87 @@ function Form() {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
 
+  const user = {
+    name:"",
+    email:"",
+    password:"",
+    gender:"",
+  }
+
+  // Capture data from input 
+ const [formData,setFormData]= useState(user);
+ const [UserData,SetUserData] = useState([]);
+//  console.log(formData)
+ function handleChange(e){
+   const {name,value} = e.target;
+   setFormData({...formData,[name]:value})
+ }
+
+
+function Handsubmit(e){
+  e.preventDefault();
+   SetUserData([...UserData,formData]);
+   setFormData(user);
+}
+console.log(UserData)
+
+
   return (
-    <div className={styles["form-container"]}>
-      <h1>ADD User</h1>
-      <form>
-        <input placeholder="Name" type="text" />
-        <input placeholder="Email" type="email" />
-        <input
-          placeholder="Password"
-          type={showPassword ? "text" : "password"}
-        />
-        <div className={styles["checkbox-container"]}>
+    <div>
+      <div className={styles["form-container"]}>
+        <h1>ADD User</h1>
+        <form onSubmit={Handsubmit}>
           <input
-            type="checkbox"
-            id="showPassword"
-            className={styles["checkbox-input"]}
-            checked={showPassword}
-            onChange={handleTogglePassword}
+            placeholder="Name"
+            type="text"
+            name="name"
+            onChange={handleChange}
           />
-          <label
-            htmlFor="showPassword"
-            className={`${styles["checkbox-custom"]} ${
-              showPassword ? styles.checked : ""
-            }`}
+          <input
+            placeholder="Email"
+            type="email"
+            name="email"
+            onChange={handleChange}
           />
-          <label htmlFor="showPassword" className={styles["checkbox-label"]}>
-            Show Password
-          </label>
-        </div>
-        <select>
-          <option value="">Gender</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-        <input
-          type="submit"
-          value="Submit"
-          className={styles["submit-button"]}
-        />
-      </form>
+          <input
+            placeholder="Password"
+            type={showPassword ? "text" : "password"}
+            name="password"
+            onChange={handleChange}
+          />
+          <div className={styles["checkbox-container"]}>
+            <input
+              type="checkbox"
+              id="showPassword"
+              className={styles["checkbox-input"]}
+              checked={showPassword}
+              onChange={handleTogglePassword}
+            />
+            <label
+              htmlFor="showPassword"
+              className={`${styles["checkbox-custom"]} ${
+                showPassword ? styles.checked : ""
+              }`}
+            />
+            <label htmlFor="showPassword" className={styles["checkbox-label"]}>
+              Show Password
+            </label>
+          </div>
+          <select name="gender" onChange={handleChange}>
+            <option value="">Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          <input
+            type="submit"
+            value="Submit"
+            className={styles["submit-button"]}
+          />
+        </form>
+      </div>
+      <div className={styles.table}>
+        <Table UserData={UserData} />
+      </div>
     </div>
   );
 }
--- React/day6/formL1/src/Componants/Table.jsx
+++ React/day6/formL1/src/Componants/Table.jsx
@@ -1,8 +1,35 @@
-
-function Table() {
+import styles from "./Table.module.css";
+function Table({ UserData }) {
   return (
-    <div>Table</div>
-  )
+    <div>
+      <>
+        <div className={styles.container}>
+          <table className={styles.table}>
+            <thead>
+              <tr className={styles.tableRow}>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Gender</th>
+              </tr>
+            </thead>
+            <tbody>
+              {UserData.map((ele, index) => (
+                <>
+                  <tr key={index}>
+                    <td>{ele.name}</td>
+                    <td>{ele.email}</td>
+                    <td>{ele.password}</td>
+                    <td>{ele.gender}</td>
+                  </tr>
+                </>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    </div>
+  );
 }
 
-export default Table
+export default Table;
